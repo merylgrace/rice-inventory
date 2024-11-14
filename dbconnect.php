bc5e9eb55612bde883d72d77091a6da0f2fe0ec0
emerita <luntian429.21@gmail.com>
2024-11-14 14:05:08 +0800

    deleted some lines

--- dbconnect.php
+++ dbconnect.php
@@ -5,9 +5,7 @@ $username = "root";
 $password = "";             
 $dbname = "rice_inventory"; // ngalan sa akong database
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
